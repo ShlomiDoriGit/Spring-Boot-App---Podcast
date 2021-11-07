22162d28aa9b5440142545348facdee267fb4f4d
Haimov <daniel.haimo58@gmail.com>
2021-11-07 22:35:28 +0200

    some anotations @ fixing

--- src/main/java/integrative/ActivitiesAPI/ActivityController.java
+++ src/main/java/integrative/ActivitiesAPI/ActivityController.java
@@ -1,7 +1,5 @@
 package integrative.ActivitiesAPI;
 
-import java.util.Date;
-import java.util.Map;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,29 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import integrative.InstancesAPI.InstanceIdBoundary;
-import integrative.UsersRelatedAPI.UserIdBoundary;
-
-
 @RestController
 public class ActivityController {
 
-	
-	
-	//Input - Operation Boundary with null operationId
-	//Output - Any JSON
+	// Input - Operation Boundary with null operationId
+	// Output - Any JSON
 
-	
-		@RequestMapping(path = "/iob/activities",
+	@RequestMapping(path = "/iob/activities",
 				method = RequestMethod.POST, 
 				produces = MediaType.APPLICATION_JSON_VALUE,
 				consumes = MediaType.APPLICATION_JSON_VALUE)
 		public ActivityBoundary invokeAnInstanceActivity(@RequestBody ActivityBoundary activity) {
-
-					activity.setActivityId(new ActivityIdBoundary("123","domain");
-					
-			return activity;
+		activity.setActivityId(new ActivityIdBoundary("123","domain"));	
+		return activity;
 		}
-	
 
 }
--- src/main/java/integrative/InstancesAPI/InsancesController.java
+++ src/main/java/integrative/InstancesAPI/InsancesController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import integrative.Boundaries.Location;
 import integrative.UsersRelatedAPI.UserIdBoundary;
 
 
-
-@RequestMapping
+@RestController
 public class InsancesController {
 
 	@RequestMapping(
