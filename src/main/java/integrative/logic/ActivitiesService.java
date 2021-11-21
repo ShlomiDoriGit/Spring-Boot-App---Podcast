f93759bf08c326197b86f4508d9a11b262066e06
Omer <Omer@DESKTOP-RKH5IKR>
2021-11-21 18:20:47 +0200

    Omers' commit 21/11/2021 18:20. ActivitiesService interface were fully implemented. ActivitiesServiceMockup were partly implemented though.

--- src/main/java/integrative/logic/ActivitiesService.java
+++ src/main/java/integrative/logic/ActivitiesService.java
@@ -1,5 +1,15 @@
 package integrative.logic;
 
+import java.util.List;
+
+import integrative.ActivitiesAPI.ActivityBoundary;
+
 public interface ActivitiesService {
+
+	public Object invokeActivity(ActivityBoundary activity);
+
+	public List<ActivityBoundary> getAllActivities(String adminSpace, String adminEmail);
+
+	public void deleteAllActivities(String adminSpace, String adminEmail);
 
 }
--- src/main/java/integrative/servicesMockup/ActivitiesServiceMockup.java
+++ src/main/java/integrative/servicesMockup/ActivitiesServiceMockup.java
@@ -1,7 +1,50 @@
 package integrative.servicesMockup;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import integrative.ActivitiesAPI.ActivityBoundary;
+import integrative.converters.ActivityConverter;
+import integrative.data.ActivityEntity;
 import integrative.logic.ActivitiesService;
 
 public class ActivitiesServiceMockup implements ActivitiesService {
+
+	private Map<String, ActivityEntity> activities;
+	private ActivityConverter activityConverter;
+
+	//Constructor
+	public ActivitiesServiceMockup(ActivityConverter converter) {
+		super();
+		this.activities=Collections.synchronizedMap(new HashMap<>());
+	}
+
+	@Autowired
+	public void setConverter(ActivityConverter activityConverter) {
+		this.activityConverter = activityConverter;
+	}
+	
+	
+	@Override
+	public Object invokeActivity(ActivityBoundary activity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ActivityBoundary> getAllActivities(String adminSpace, String adminEmail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAllActivities(String adminSpace, String adminEmail) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
