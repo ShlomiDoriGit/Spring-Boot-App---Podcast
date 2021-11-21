b09d30cbc1e9dfea94dacf3366050fe9fa370be3
Stav <stav.rabinovich@s.afeka.ac.il>
2021-11-21 14:57:28 +0200

    ActivityEntity, ActivityConventor

--- src/main/java/integrative/converters/ActivityConverter.java
+++ src/main/java/integrative/converters/ActivityConverter.java
@@ -3,7 +3,10 @@ package integrative.converters;
 import org.springframework.stereotype.Component;
 
 import integrative.ActivitiesAPI.ActivityBoundary;
+import integrative.ActivitiesAPI.InvokeBy;
+import integrative.UsersRelatedAPI.UserId;
 import integrative.data.ActivityEntity;
+import integrative.data.UserRole;
 
 
 @Component
@@ -11,14 +14,53 @@ public class ActivityConverter {
 	
 	public ActivityEntity convertToEntity(ActivityBoundary boundary) {
 		ActivityEntity entity = new ActivityEntity();
-		//TODO
+		
+		entity.setActivityId(boundary.getActivityId());
+		
+		if (boundary.getType() != null) {
+			entity.setType(boundary.getType());
+		}else {
+			entity.setType(UserRole.PLAYER.name());
+		}
+
+		entity.setInstance(boundary.getInstance());
+		if (boundary.getInstance() != null) { // ? Can be null?
+		entity.setInstance(boundary.getInstance());
+		}else {
+			entity.setType("NO_INSTANCE");
+		}
+
+		entity.setCreatedTimestamp(boundary.getCreatedTimestamp());
+
+		entity.setInvokedBy(boundary.getInvokedBy());
+		
+		if (boundary.getInvokedBy().getUserId() != null) {
+			if (boundary.getInvokedBy().getUserId().getDomain() != null){
+				entity.setInvokedBy(boundary.getInvokedBy());
+//				entity.setInvokedBy().setDomain(boundary.getInvokedBy().getUserId().getDomain());
+			}
+		}
+		
+		entity.setActivityAttributes(boundary.getActivityAttributes());
+
 		return entity;
 	}
 	
 	public ActivityBoundary convertToBoundary(ActivityEntity entity) {
 		ActivityBoundary boundary = new ActivityBoundary();
-		//TODO
+		
+		boundary.setActivityId(entity.getActivityId());		
+		boundary.setType(entity.getType());
+//		boundary.setInstance(
+//			new InvokeBy( new UserId(
+//					entity.getDomain(),
+//					entity.getEmail())));
+//				entity.getFirstName(), 
+//				entity.getLastName())); 
+		boundary.setCreatedTimestamp(entity.getCreatedTimestamp());
+		boundary.setInvokedBy(entity.getInvokedBy());
+		boundary.setActivityAttributes(entity.getActivityAttributes());
+			
 		return boundary;
 	}
-
 }
--- src/main/java/integrative/data/ActivityEntity.java
+++ src/main/java/integrative/data/ActivityEntity.java
@@ -1,5 +1,66 @@
 package integrative.data;
 
-public class ActivityEntity {
+import java.util.Date;
+import java.util.Map;
 
+import integrative.ActivitiesAPI.ActivityId;
+import integrative.ActivitiesAPI.ActivityInstance;
+import integrative.ActivitiesAPI.InvokeBy;
+
+public class ActivityEntity {
+	private ActivityId activityId;
+	private String type;
+	private ActivityInstance instance;
+	private Date createdTimestamp;
+	private InvokeBy invokedBy;
+	private Map<String, Object> activityAttributes;
+	
+	public ActivityEntity() {
+		super();
+	}
+	
+	public ActivityId getActivityId() {
+		return activityId;
+	}
+	public void setActivityId(ActivityId activityId) {
+		this.activityId = activityId;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public ActivityInstance getInstance() {
+		return instance;
+	}
+	public void setInstance(ActivityInstance instance) {
+		this.instance = instance;
+	}
+	public Date getCreatedTimestamp() {
+		return createdTimestamp;
+	}
+	public void setCreatedTimestamp(Date createdTimestamp) {
+		this.createdTimestamp = createdTimestamp;
+	}
+	public InvokeBy getInvokedBy() {
+		return invokedBy;
+	}
+	public void setInvokedBy(InvokeBy invokedBy) {
+		this.invokedBy = invokedBy;
+	}
+	public Map<String, Object> getActivityAttributes() {
+		return activityAttributes;
+	}
+	public void setActivityAttributes(Map<String, Object> activityAttributes) {
+		this.activityAttributes = activityAttributes;
+	}
+
+	@Override
+	public String toString() {
+		return "ActivityEntity [activityId=" + activityId + ", type=" + type + ", instance=" + instance
+				+ ", createdTimestamp=" + createdTimestamp + ", invokedBy=" + invokedBy + ", activityAttributes="
+				+ activityAttributes + "]";
+	}
+	
 }
